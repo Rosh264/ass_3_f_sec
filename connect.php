e50106bcfe1c7cb4e9f425ffba980d88a4273d43
Rosh264 <23btre136@gcu.edu.in>
2024-07-11 08:40:14 +0530

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername = "localhost";
+$username = "admin1";
+$password = "Rosh123";
+$dbname = "products";
+  
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully!";
+$conn->close();
+?>
